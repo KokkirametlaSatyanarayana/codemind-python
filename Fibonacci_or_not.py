60ed1cefc40153fc6abd3378355f6b7e4defc5ef
KokkirametlaSatyanarayana <97425594+KokkirametlaSatyanarayana@users.noreply.github.com>
2022-06-09 23:56:42 +0530

    n=int(input()) a=0 count=0 b=1 while n>0:     c=a+b     a=b     b=c     if b==n:         count+=1         break     elif b>n:         break
    if count>0:
        print(True)
    else:
        print(False)

---
+++ Fibonacci_or_not.py
@@ -0,0 +1,20 @@
+n=int(input())
+a=0
+count=0
+b=1
+while n>0:
+    c=a+b
+    a=b
+    b=c
+    if b==n:
+        count+=1
+        break
+    elif b>n:
+        break
+        
+if count>0:
+    print(True)
+else:
+    print(False)
+    
+    
