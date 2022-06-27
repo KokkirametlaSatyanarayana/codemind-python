9a9f6a248607f31abfb4161784e40b0c099a1d1f
KokkirametlaSatyanarayana <97425594+KokkirametlaSatyanarayana@users.noreply.github.com>
2022-06-27 23:45:59 +0530

    a=int(input()) l=list(map(int,input().split())) b=int(input()) y=max(l) for i in range(a):         if l[i]+b<y:             print(False,end=' ')         else:             print(True,end=' ')

---
+++ Greatest_Number_of_Candies.py
@@ -0,0 +1,17 @@
+a=int(input())
+l=list(map(int,input().split()))
+b=int(input())
+y=max(l)
+for i in range(a):
+        if l[i]+b<y:
+            print(False,end=' ')
+        else:
+            print(True,end=' ')
+            
+
+
+    
+    
+    
+    
+    
