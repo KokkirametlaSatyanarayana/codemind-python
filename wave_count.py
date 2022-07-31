9735191bcd5028c81c9259e05311f4eb449173d3
KokkirametlaSatyanarayana <97425594+KokkirametlaSatyanarayana@users.noreply.github.com>
2022-07-31 21:16:07 +0530

    a=int(input()) b=list(map(int,input().split())) count=0 for i in range(1,a-1):     if (b[i]>b[i-1] and b[i]>b[i+1]):         count+=1 if count==(a-1)//2:     print(count) else:     print('-1')

--- wave_count.py
+++ wave_count.py
@@ -2,7 +2,7 @@ a=int(input())
 b=list(map(int,input().split()))
 count=0
 for i in range(1,a-1):
-    if b[i]>b[i-1] and b[i]>b[i+1]:
+    if (b[i]>b[i-1] and b[i]>b[i+1]):
         count+=1
 if count==(a-1)//2:
     print(count)
