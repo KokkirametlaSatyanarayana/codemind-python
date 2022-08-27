1e099a62a109dc20ed5e0f85031ded8f3eb89aa6
KokkirametlaSatyanarayana <97425594+KokkirametlaSatyanarayana@users.noreply.github.com>
2022-08-27 23:44:53 +0530

    a=input() a=a.lower() x=set(a) y=list(x) count=0 for i in range(len(y)):     if ord(y[i])!=32:         count+=1 if count==26:     print(True) else:     print(False)

---
+++ Pangram_or_not.py
@@ -0,0 +1,13 @@
+a=input()
+a=a.lower()
+x=set(a)
+y=list(x)
+count=0
+for i in range(len(y)):
+    if ord(y[i])!=32:
+        count+=1
+if count==26:
+    print(True)
+else:
+    print(False)
+        
