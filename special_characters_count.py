f9abdd5f1339288b842a1a51599081c0fe951eaf
KokkirametlaSatyanarayana <97425594+KokkirametlaSatyanarayana@users.noreply.github.com>
2022-08-27 23:09:23 +0530

    a=input() count=0 for i in range(len(a)):     if (ord(a[i])>=48 and ord(a[i])<=57) or (ord(a[i])>=65 and ord(a[i])<=90) or (ord(a[i])>=97 and ord(a[i])<=122) or ord(a[i])==32:         count+=1 print(len(a)-count)

--- special_characters_count.py
+++ special_characters_count.py
@@ -4,4 +4,3 @@ for i in range(len(a)):
     if (ord(a[i])>=48 and ord(a[i])<=57) or (ord(a[i])>=65 and ord(a[i])<=90) or (ord(a[i])>=97 and ord(a[i])<=122) or ord(a[i])==32:
         count+=1
 print(len(a)-count)
-        
