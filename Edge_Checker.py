8e8493198a2944c1e491d83d712c354048b39020
KokkirametlaSatyanarayana <97425594+KokkirametlaSatyanarayana@users.noreply.github.com>
2022-10-13 14:43:36 +0530

    a,b=map(int,input().split()) if abs(b-a)==1 or abs(a-b)==9:     print("Yes") else:     print("No")

--- Edge_Checker.py
+++ Edge_Checker.py
@@ -1,8 +1,5 @@
 a,b=map(int,input().split())
-if a==b+1 or a==b-1:
-    print('Yes')
-elif a==b+9 or a==b-9:
-    print('Yes')
+if abs(b-a)==1 or abs(a-b)==9:
+    print("Yes")
 else:
-    print('No')
-    
+    print("No")
